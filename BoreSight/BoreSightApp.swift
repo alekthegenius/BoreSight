c2ef37ce3f7a388bc9c91659b4eb2ec2e2666b35
Alek V <alekvasek@icloud.com>
2025-08-05 14:45:38 -0500

    Initial Commit

---
+++ BoreSight/BoreSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoreSightApp.swift
+//  BoreSight
+//
+//  Created by Alek Vasek on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BoreSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoreSight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BoreSight
+//
+//  Created by Alek Vasek on 8/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
